338e9f44323027cdca933a14bdde98cbd7715677
Aneta Crescuicci <anetacres@gmail.com>
2018-11-06 22:02:34 +0100

    2nd challenge

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:title, :details, :completed))
+    @task.save
+
+    redirect_to alltasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params.require(:task).permit(:title, :details, :completed))
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to alltasks_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+#CREATE a task - get a form, create new
+get "tasks/new", to: "tasks#new"
+post "tasks", to: "tasks#create"
+
+# READ all tasks
+get "/tasks", to: "tasks#index", as: "alltasks"
+#READ a task
+get "/tasks/:id", to: "tasks#show", as: "task"
+
+#UPDATE a task - get a form for id, update
+get "/tasks/:id/edit", to: "tasks#edit", as: "edittask"
+patch "tasks/:id", to: "tasks#update"
+
+
+# DESTROY
+delete "/tasks/:id", to: "tasks#destroy", as: "deletetask"
 end
